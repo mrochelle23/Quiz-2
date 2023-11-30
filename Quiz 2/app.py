b8eaffbcac13702006e61425b1cd7071a29acbcd
mrochelle23 <142530319+mrochelle23@users.noreply.github.com>
2023-11-30 14:01:28 -0600

    Update app.py

--- Quiz 2/app.py
+++ Quiz 2/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-import json
 import requests
 from pprint import PrettyPrinter
 
@@ -9,23 +8,22 @@ app = Flask(__name__)
 def homePage():
     return render_template('swapi_form.html')
 
-SWAPI_URL = "https://swapi.py4e.com/api/"
+SWAPI_URL = "https://swapi.py4e.com/api/people"
 pp = PrettyPrinter(indent=4)
 
 @app.route('/swapi', methods=['GET', 'POST'])
 def swapi():
     if request.method == 'POST':
-        category = request.form.get('category')
         character_id = request.form.get('character_id')
 
-        response = requests.get(f'{SWAPI_URL}{category}/{character_id}/')
+        response = requests.get(f'{SWAPI_URL}/{character_id}/')
 
         record = response.json()
 
         context = {
         'record': record,
         }
-
+        print(record)
         return render_template('swapi_results.html', **context)
     else:
         context = {
